ab8d13e5f829dc939221ad7df2c80c5c58ec7153
Zero <123734065+ZeroxxID@users.noreply.github.com>
2026-03-07 21:07:48 +0700

    Update select.sql

--- GDG Studty Group/Basic SQL & Introduction to PostgreSQL/select.sql
+++ GDG Studty Group/Basic SQL & Introduction to PostgreSQL/select.sql
@@ -1,15 +1,3 @@
-SELECT * FROM orders;
-
-SELECT * FROM orders WHERE order_date BETWEEN '2024-01-01' AND '2024-01-31';
-
-SELECT * FROM orders WHERE price > 250000;
-
-SELECT * FROM orders WHERE course ILIKE '%SQL%';
-
-SELECT course, price FROM orders ORDER BY price ASC LIMIT 3;
-
-SELECT DISTINCT city FROM customers;
-
 SELECT 
     c.name, 
     c.city, 
@@ -18,14 +6,6 @@ SELECT
     o.order_date 
 FROM orders AS o 
 JOIN customers AS c ON o.customer_id = c.id;
-
-SELECT 
-    c.city, 
-    SUM(o.price) AS total_spend 
-FROM orders AS o 
-JOIN customers AS c ON o.customer_id = c.id 
-GROUP BY c.city 
-HAVING SUM(o.price) > 1000000;
 
 SELECT 
     c.name, 
